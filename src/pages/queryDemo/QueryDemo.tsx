03198bec371c8592165d97309c352cc44d713f1e
Nick Barnard <n.barnard@ctidigital.com>
2023-01-17 13:32:07 +0000

    Add cache fetch

--- src/pages/queryDemo/QueryDemo.tsx
+++ src/pages/queryDemo/QueryDemo.tsx
@@ -1,35 +1,45 @@
-import React from 'react';
-import { Alert, Box, CircularProgress, Divider, Stack, Typography } from '@mui/material';
-import { useQuery } from '@tanstack/react-query';
+import React, { useState } from 'react';
+import { Alert, Box, Button, CircularProgress, Divider, Stack, Typography } from '@mui/material';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { fetchTodoQueryConfig, fetchTodos, todoQueryKeys } from './queries';
 import { Todo } from './types';
 
 function TodoList() {
   const { isLoading, isFetching, error, data, isSuccess } = useQuery(fetchTodoQueryConfig);
-
+  
   if (error) return <Alert severity="error">Error fetching data</Alert>;
   if (isLoading || isFetching)
-    return (
-      <Box sx={{ display: 'flex' }}>
+  return (
+    <Box sx={{ display: 'flex' }}>
         <CircularProgress />
       </Box>
     );
-  return isSuccess ? (
-    <Stack>
+    return isSuccess ? (
+      <Stack>
       {data?.slice(0, 9).map((todo: Todo) => (
         <Box key={todo.id}>{todo.title}</Box>
-      ))}
+        ))}
     </Stack>
   ) : null;
 }
 
 function QueryDemo() {
+  const queryClient = useQueryClient();
+  const [showCachedData, setShowCachedData] = useState<boolean>(false);
+
+  const cachedData = queryClient.getQueryData([todoQueryKeys.TODOLIST]);
+  
   return (
     <Box sx={{ p: 2 }}>
       <Typography variant="h4">REACT-QUERY DEMO</Typography>
       <Divider sx={{ m: 2 }} />
       <Typography variant="h5">Todo List:</Typography>
       <TodoList />
+      <Divider sx={{ m: 2 }} />
+      <Typography variant="h4">CACHED DATA</Typography>
+      <Divider sx={{ m: 2 }} />
+      <Button onClick={() => setShowCachedData(!showCachedData)}>SHOW DATA</Button>
+      {showCachedData && JSON.stringify(cachedData)}
     </Box>
   );
 }
