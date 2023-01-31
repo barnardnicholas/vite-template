03043c1c8e8286808f13321b9a42c49812f5332a
Nick Barnard <n.barnard@ctidigital.com>
2023-01-31 09:02:26 +0000

    Updates

--- src/pages/queryDemo/QueryDemo.tsx
+++ src/pages/queryDemo/QueryDemo.tsx
@@ -10,11 +10,17 @@ import {
   Typography,
 } from '@mui/material';
 import { useQuery, useQueryClient } from '@tanstack/react-query';
-import { fetchTodoQueryConfig, todoQueryKeys } from './queries';
+import { fetchTodoQueryConfig, fetchTodos, todoQueryKeys } from './queries';
 import { Todo } from './types';
 
 function TodoList() {
-  const { isLoading, isFetching, error, data, isSuccess } = useQuery(fetchTodoQueryConfig);
+  const { isLoading, isFetching, error, data, isSuccess } = useQuery({
+    queryKey: [todoQueryKeys.TODOLIST],
+    queryFn: fetchTodos,
+    // staleTime: 60000, // Don't refetch this query for 1 minute
+    // cacheTime: 120000, // keep unused data in cache for 2 minutes, then delete
+    // refetchInterval: 180000, // Refetch this data every 3 minutes while query is in use
+  });
 
   if (error) return <Alert severity="error">Error fetching data</Alert>;
   if (isLoading || isFetching)
