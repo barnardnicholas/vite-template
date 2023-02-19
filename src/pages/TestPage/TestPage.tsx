0637e1a9bde9d747651abfecfdae59a370218ae5
Nick Barnard <n.barnard@ctidigital.com>
2023-02-19 15:14:00 +0000

    Add chips to TestPage

--- src/pages/TestPage/TestPage.tsx
+++ src/pages/TestPage/TestPage.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
-import { Container, Typography } from '@mui/material';
+import { Box, Chip, Container, Typography } from '@mui/material';
 import { loremIpsum } from './constants';
 
 function TestPage() {
   return (
     <Container>
-      <Typography variant="h1">Page</Typography>
+      <Typography variant="h1">H1 Title</Typography>
       {loremIpsum.map((text: string) => (
         <Typography variant="body1" key={text.slice(0, 9)}>
           {text}
         </Typography>
       ))}
+      <Box>
+        {['Chip', 'Chip', 'Chip', 'Chip'].map((label: string, i: number) => (
+          <Chip
+            key={`${label}-${i}`}
+            label="Chip"
+            clickable
+            onDelete={() => console.log(`Delete ${label}`)}
+            sx={{ mr: 1 }}
+            onClick={() => console.log(`Click ${label}`)}
+          />
+        ))}
+      </Box>
     </Container>
   );
 }
